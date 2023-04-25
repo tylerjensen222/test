278e569a679b5f8dbbbee7f3653f104946a87377
tylerjensen222 <125998592+tylerjensen222@users.noreply.github.com>
2023-04-25 17:53:55 -0600

    print pms

--- print_pms.py
+++ print_pms.py
@@ -1,4 +1,8 @@
-pms_list = ["entrata", "realpage", "rentcafe"]
 
-for pms in pms_list:
-    print(pms)
+def print(pms):
+    pms_list = ["entrata", "realpage", "rentcafe"]
+
+    for pms in pms_list:
+        print(pms)
+
+    
