1911d218c5125ef1f674e964289727bd5d19269f
tylerjensen222 <125998592+tylerjensen222@users.noreply.github.com>
2023-04-26 09:29:32 -0600

    edits

--- print_pms.py
+++ print_pms.py
@@ -1,8 +1,17 @@
 
-def print(pms):
+def print_pms():
+
     pms_list = ["entrata", "realpage", "rentcafe"]
 
     for pms in pms_list:
         print(pms)
+    
+    return
+
+
+
+
+if __name__ == '__main__':
+    print_pms()
 
     
